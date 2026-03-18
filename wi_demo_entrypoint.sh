4d88148be8cad3ddb1a17190ac93e686d16e3550
m.ciofo <m.ciofo@ipzs.it>
2026-03-18 12:48:49 +0100

    Add echo for entrypoint-sh

--- wi_demo_entrypoint.sh
+++ wi_demo_entrypoint.sh
@@ -1,7 +1,15 @@
+#!/bin/sh
+
+echo "ENTRYPOINT WI"
+
+echo "CREATION ENVIRONMENT"
 python -m venv venv
 
+echo "ACTIVATION ENVIRONMENT"
 source venv/bin/activate
 
+echo "INSTALL ALL DEPENDENCY"
 pip install .
 
+echo "RUN APPLICATION"
 python app.py
