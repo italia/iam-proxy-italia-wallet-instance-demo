7d942c2f77017bc1baab4f0211e47666edd5526d
manpace <221346462+manpace@users.noreply.github.com>
2026-03-23 14:13:31 +0100

    update poetry.lock migrate EC from wallet_solution to wallet_provider

--- app/models/metadata/1_3_3/metadata.py
+++ app/models/metadata/1_3_3/metadata.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 
 from .federation_entity import FederationEntity
-from .wallet_solution import WalletSolution
+from .wallet_provider import WalletProvider
 
 
 class MetadataGroup(BaseModel):
     federation_entity: FederationEntity
-    wallet_solution: WalletSolution
+    wallet_provider: WalletProvider
