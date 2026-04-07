f07e559321ff17f29efdb6bc6b42606bf6a8a056
manpace <221346462+manpace@users.noreply.github.com>
2026-04-07 15:53:48 +0200

    revert metadata type: wallet_provider to wallet_solution

--- app/models/metadata/1_3_3/metadata.py
+++ app/models/metadata/1_3_3/metadata.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 
 from .federation_entity import FederationEntity
-from .wallet_provider import WalletProvider
+from .wallet_solution import WalletSolution
 
 
 class MetadataGroup(BaseModel):
     federation_entity: FederationEntity
-    wallet_provider: WalletProvider
+    wallet_solution: WalletSolution
